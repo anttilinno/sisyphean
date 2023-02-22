f465d68e8efbafbcb6baca39dca82a625dba4f56
Antti <antti.linno@gmail.com>
2023-02-22 21:51:19 +0200

    Add create test

--- backend/app/app/tests/api/test_todo.py
+++ backend/app/app/tests/api/test_todo.py
@@ -1,4 +1,20 @@
+from fastapi.testclient import TestClient
+
 from app.core.config import settings
+
+
+def test_create_item(client: TestClient) -> None:
+    data = {"title": "Foo", "description": "Fighters"}
+    response = client.post(
+        f"{settings.API_V1_STR}/todos",
+        json=data,
+    )
+    assert response.status_code == 200
+    content = response.json()
+    print(content)
+    assert content["title"] == data["title"]
+    assert content["description"] == data["description"]
+    assert "id" in content
 
 
 def test_fetch_todos(client):
